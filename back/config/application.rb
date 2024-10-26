0f5ca1386b7b02a99059a3b7ce966aede33b37cd
yudai-kdix <yudai.homma222@gmail.com>
2024-10-27 01:21:13 +0900

    cors許可

--- back/config/application.rb
+++ back/config/application.rb
@@ -26,5 +26,15 @@ module Backend
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
+
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
