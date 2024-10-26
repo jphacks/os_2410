db9e3768038bb1df55771be625c5d673bbc281b8
yudai-kdix <yudai.homma222@gmail.com>
2024-10-26 22:47:55 +0900

    ユーザーのラストログインのアップデート

--- back/app/services/user_login_check.rb
+++ back/app/services/user_login_check.rb
@@ -9,9 +9,11 @@ def check_first_login(user)
   # ユーザーがログインした日付が今日の日付と一致するかどうかを判定
   if login_date == Date.today
     # 一致する場合はfalseを返す
+    user.update(last_login: Date.today)
     return false
   else
     # 一致しない場合はtrueを返す
+    user.update(last_login: Date.today)
     return true
   end
 end
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_26_052610) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_26_130843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_26_052610) do
     t.float "health_points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "hp_movement"
+    t.float "lifespan_movement"
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
@@ -55,8 +57,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_26_052610) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.json "tokens"
+    t.datetime "last_login"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "action_logs", "characters"
