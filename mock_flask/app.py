9e02b02011d26c46a6da9672325a35b80628888e
atnuhs <syunnta0033@gmail.com>
2024-10-26 11:16:44 +0900

    fix: mockのapp.pyにCORSを追加

--- mock_flask/app.py
+++ mock_flask/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 import datetime
+from flask_cors import CORS
 from character_manage import check_dead
 app = Flask(__name__)
+CORS(app)
 
 # モックデータ
 users = [
